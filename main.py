8243bb0f23e1f24cd9388b3cfc407f10a73b2e23
Andrea Waltlová <andywaltlova@gmail.com>
2021-02-27 12:34:51 +0100

    Update main script to merge all data sources

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from sources.world_bank import get_countries, get_alcohol_consumption
-from sources.world_bank import get_cause_of_death_by_road_traffic_injury, \
-    get_cause_of_death_by_injury
 from sources.google_sheets import auth, write_df
+from sources.world_bank import get_countries, get_alcohol_consumption
+from sources.world_bank import get_cause_of_death_by_road_traffic_injury
+from sources.world_bank import get_cause_of_death_by_injury
+from sources.hapiness_report import get_world_hapiness
+
 import os.path
 import pandas as pd
 
@@ -9,22 +11,27 @@ SHEET_ID = '1tKdtPdbDPfQNmaBYv1l0SbUUEA_7rzt_BMAnHcJhHqo'
 
 if __name__ == '__main__':
     service = auth()
+
+    # WorldBank sources
     countries = get_countries()
     alcohol = get_alcohol_consumption()
     injuries = get_cause_of_death_by_injury()
     rtraffic_injuries = get_cause_of_death_by_road_traffic_injury()
-
-    data = pd.merge(countries, alcohol, how='inner', on=['name'])
-    data = pd.merge(data, injuries, how='inner', on=['name'])
-    data = pd.merge(data, rtraffic_injuries, how='inner', on=['name'])
-
-    # individual sources
     countries.to_csv(os.path.expanduser('data/countries.csv'))
     alcohol.to_csv(os.path.expanduser('data/alcohol.csv'))
     injuries.to_csv(os.path.expanduser('data/injuries.csv'))
     rtraffic_injuries.to_csv(os.path.expanduser('data/rtraffic_injuries.csv'))
 
-    # combined
-    data = data.dropna()
+    # Kaggle
+    hapiness = get_world_hapiness()
+
+    # Combined
+    data = pd.merge(countries, alcohol, how='left', on=['name'])
+    data = pd.merge(data, injuries, how='left', on=['name'])
+    data = pd.merge(data, rtraffic_injuries, how='left', on=['name'])
+    data = pd.merge(data, hapiness, how='left', on=['name'])
+
+    # data = data.dropna()
+    data = data.fillna('')
     data.to_csv(os.path.expanduser('data/data.csv'))
     write_df(service, SHEET_ID, 'A1', data)
