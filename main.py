7e4e76022ce57ae4b311c34c2d25868447e6f4be
Andrea Waltlová <andywaltlova@gmail.com>
2021-02-27 17:09:09 +0100

    Added drinking driving limit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sources.world_bank import get_countries, get_alcohol_consumption
 from sources.world_bank import get_cause_of_death_by_road_traffic_injury
 from sources.world_bank import get_cause_of_death_by_injury
 from sources.hapiness_report import get_world_hapiness
-from sources.wikipedia_tables import get_legal_driking_age
+from sources.wikipedia_tables import get_legal_driking_age, get_driving_legal_limit
 from sources.translations import create_complete_df
 
 import os.path
@@ -30,12 +30,13 @@ if __name__ == '__main__':
     injuries = get_cause_of_death_by_injury()
     rtraffic_injuries = get_cause_of_death_by_road_traffic_injury()
     drinking_age = get_legal_driking_age()
+    driving_limit = get_driving_legal_limit()
 
     # Kaggle
     hapiness = get_world_hapiness()
 
     # Google translation api
-    translation_df = create_complete_df()
+    #translation_df = create_complete_df()
 
     # Combined
     data = pd.merge(countries, alcohol, how='left', on=['name'])
@@ -43,11 +44,13 @@ if __name__ == '__main__':
     data = pd.merge(data, rtraffic_injuries, how='left', on=['name'])
     data = pd.merge(data, hapiness, how='left', on=['name'])
     data = pd.merge(data, drinking_age, how='left', on=['name'])
+    data = pd.merge(data, driving_limit, how='left', on=['name'])
 
     data = fill_na_mean(data)
+    data = data.fillna('')
+    print(data.head())
 
     data.to_csv(os.path.expanduser('data/data.csv'))
 
     write_df(service, SHEET_ID, 'A1', data)
-
-    write_df(service, SHEET_ID_trans, 'A1', translation_df)
+    # write_df(service, SHEET_ID_trans, 'A1', translation_df)
--- sources/wikipedia_tables.py
+++ sources/wikipedia_tables.py
@@ -13,32 +13,41 @@ def get_legal_driking_age():
                   'purchase age off-premise', 'notes']
     df = df.iloc[:, [0, 1, 4, 5, 6]]
 
-    df['purchase age on-premise'] = [re.sub('\[(.*)\]', '', x) for x in
-                                     df['purchase age on-premise']]
-    df['purchase age off-premise'] = [re.sub('\[(.*)\]', '', x) for x in
-                                      df['purchase age off-premise']]
+    names = ['purchase age on-premise', 'purchase age off-premise']
+    for name in names:
+        df[name] = [re.sub('\[(.*)\]', '', x).split(' ')[0].split('(')[0] for x
+                    in df['purchase age on-premise']]
 
-    df['Administrative division'] = df['Administrative division'].replace(
-        'Switzerland (federal law)',
-        'Switzerland')
-    df['Administrative division'] = df['Administrative division'].replace(
-        'United Kingdom',
-        'United Kingdom general')
+    df['notes'] = df['notes'].fillna('')
+    df['notes'] = [re.sub('\[(.*)\]', '', x) for x in df['notes']]
 
-    df['Administrative division'] = df['Administrative division'].replace(
-        'Czechia',
-        'Czech Republic')
-    df['name'] = df['name'].replace('Czechia',
-                                    'Czech Republic')
-    df['Administrative division'] = df['Administrative division'].replace(
-        'Slovakia',
-        'Slovak Republic')
-    df['name'] = df['name'].replace('Slovakia',
-                                    'Slovak Republic')
+    col_name = 'Administrative division'
+    to_replace = {
+        'Switzerland (federal law)': 'Switzerland',
+        'United Kingdom': 'United Kingdom general',
+        'Czechia': 'Czech Republic',
+        'Slovakia': 'Slovak Republic'
+    }
+    for old, new in to_replace.items():
+        df[col_name] = df[col_name].replace(old, new)
+
+    df['name'] = df['name'].replace('Czechia', 'Czech Republic')
+    df['name'] = df['name'].replace('Slovakia', 'Slovak Republic')
+
     df = df[(df['name'] == df['Administrative division']) | (
             df['Administrative division'] == 'England Wales')]
-    df['notes'] = df['notes'].fillna('')
+
     df = df.drop('Administrative division', axis=1)
     df.to_csv(os.path.expanduser('data/drinking_age.csv'))
     return df
 
+
+def get_driving_legal_limit():
+    with open(os.path.expanduser('data\driving_limits.txt')) as f:
+        lines = [re.sub('\[(.*)\]', '', l.strip()) for l in f.readlines()]
+        countries = [l.split(': ')[0] for l in lines]
+        limits = [l.split(': ')[1] if len(l.split(': ')) > 1 else '' for l in
+                  lines]
+
+    res = pd.DataFrame({'name': countries, 'Drunk driving limit': limits})
+    return res
